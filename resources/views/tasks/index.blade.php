114721a135e7a629b9e67e1bcce17217874d66c8
mimpski <premiermac3@Premiers-iMac.local>
2016-03-31 10:38:43 +0100

    first commit

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+                <div class="title">Tasks index</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+                <div class="title">Laravel 5</div>
+@endsection
